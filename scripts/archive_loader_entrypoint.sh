416e45f4229b986db7ad9f9eb14a4d278d39c390
Martijn van Exel <m@rtijn.org>
2025-02-12 16:21:28 -0700

    refactor: Simplify archive loader entrypoint script

--- scripts/archive_loader_entrypoint.sh
+++ scripts/archive_loader_entrypoint.sh
@@ -1,18 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 set -e
-
-# Check if input file is provided
-if [ -z "$1" ]; then
-  echo "Usage: docker-compose run archive_loader <input_file.osm.bz> [--from_date YYYYMMDD] [--to_date YYYYMMDD]"
-  exit 1
-fi
-
-# Construct the database URL
-DB_URL="postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}"
-
-echo $DB_URL
-
-# Run the archive loader with the provided arguments
-python /app/scripts/archive_loader.py "/data/$1" "$DB_URL" "${@:2}"
-
-echo "Archive loading completed successfully"
+# Forward all arguments to the archive loader script.
+python /app/archive_loader.py "$@"
