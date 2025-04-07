cee93104c03252f8ffc3aef7cb5066e0f9920e88
Dimitar <19364673+Dimitar5555@users.noreply.github.com>
2025-04-07 11:20:40 +0300

    Use preferred tile.openstreetmap.org URL
    Ref: https://github.com/openstreetmap/operations/issues/737

--- osm_meet_your_mappers/static/script.js
+++ osm_meet_your_mappers/static/script.js
@@ -565,7 +565,7 @@ let map, drawnItems, drawRectangle, drawPolygon;
 
 function initializeMap() {
   map = L.map("map", { zoomControl: false }).setView([51.505, -0.09], 13);
-  L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "&copy; OpenStreetMap contributors",
   }).addTo(map);
 
